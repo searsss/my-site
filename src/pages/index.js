338440c22608b1a3e08d0496e45d0936422d2038
searsss <danieltsears@gmail.com>
2018-08-23 14:55:23 -0700

    setting up layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,16 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Learn to Design and code React apps</h1>
-    <p>Welcome to your new Gatsby site my friend.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="hero">
+      <div className="heroGroup">
+        <h1>Learn to Design and code React apps</h1>
+        <p>Welcome to your new Gatsby site my friend.</p>
+        <p>Now go build something great.</p>
+        <Link to="/page-2/">Go to page 2</Link>
+      </div>
+    </div>
+
+    
   </div>
 )
 
